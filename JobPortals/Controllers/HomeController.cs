7cee8e6ed792156156dcdbbd4e939be01be3b15a
Niranjanpaswan1511 <Niranjanpaswan1511@gmail.com>
2023-12-28 23:34:39 +0530

    Add project files.

---
+++ JobPortals/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortals.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult about()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult jobdetails() 
+        {
+            return View();
+        }
+        public ActionResult jobListings()
+        {
+            return View();
+        }
+    }
+}
